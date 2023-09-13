27015a68f156d2beaee6726d2288e66edafe5c75
Iamneo <techadmin@iamneo.ai>
2023-09-13 06:34:19 +0000

    Backup Commit

---
+++ create_folder_and_file.sh
@@ -0,0 +1,7 @@
+directory_name="my_new_folder"
+file_name="my_new_file.txt"
+if [ -d "/home/coder/$directory_name" ]; then
+    echo "Directory already exists."
+else
+    mkdir "/home/coder/$directory_name"
+    echo "Directory '$directory_name' created successfully."
